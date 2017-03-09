256f8ce8fb705b7de0882b2ee13a85cf551705ee
Benjamin Minixhofer <bminixhofer@gmail.com>
2017-03-09 15:47:02 +0100

    added basic profanity counter available via a JSON response

--- server.py
+++ server.py
@@ -2,10 +2,15 @@ import requests
 import json
 from flask import Flask, request, Response
 
+
 app = Flask(__name__)
 api_key = '847d1383d7e8d5f9d0a685c9ee091a34'
 
-@app.route("/")
+with open('profanity_list.txt') as f:
+    profanity_list = [line.strip() for line in f.readlines()]
+
+
+@app.route('/')
 def hello():
     params = {
         'q_track': request.args.get('track'),
@@ -13,10 +18,18 @@ def hello():
         'format': 'json',
         'apikey': api_key
     }
+
     r = requests.get('https://api.musixmatch.com/ws/1.1/matcher.lyrics.get', params=params)
-    data = json.loads(r.text)
+    data = r.json()
+    lyrics_text = data['message']['body']['lyrics']['lyrics_body']
+    lyrics = lyrics_text.split()
+    profanity_count = [word for word in lyrics if word in profanity_list]
     return Response(
-        data['message']['body']['lyrics']['lyrics_body'],
+        json.dumps({
+            'lyrics': lyrics_text,
+            'profanity': profanity_count,
+            'percentage': len(profanity_count) / len(lyrics) * 100
+        }),
         status=r.status_code,
         content_type='json'
     )
