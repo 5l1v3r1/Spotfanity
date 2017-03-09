f4e2156beee2867857681fb8da16c45b834b5ba8
Benjamin Minixhofer <bminixhofer@gmail.com>
2017-03-09 17:01:59 +0100

    moved api key, added static file serving test

--- server.py
+++ server.py
@@ -1,22 +1,29 @@
+from os.path import join, dirname
+from os import environ
+from dotenv import load_dotenv
 import requests
 import json
-from flask import Flask, request, Response
+from flask import Flask, request, Response, current_app
 
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 app = Flask(__name__)
-api_key = '847d1383d7e8d5f9d0a685c9ee091a34'
 
 with open('profanity_list.txt') as f:
     profanity_list = [line.strip() for line in f.readlines()]
 
-
 @app.route('/')
-def hello():
+def send_site():
+    return current_app.send_static_file('index.html')
+
+@app.route('/api/track')
+def get_track():
     params = {
         'q_track': request.args.get('track'),
         'q_artist': request.args.get('artist'),
         'format': 'json',
-        'apikey': api_key
+        'apikey': environ.get('MUSIXMATCH_KEY')
     }
 
     r = requests.get('https://api.musixmatch.com/ws/1.1/matcher.lyrics.get', params=params)
